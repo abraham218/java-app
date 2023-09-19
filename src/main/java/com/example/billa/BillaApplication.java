d597872ab669ef96899fd97329596ad9fc7e5bc3
Jenkins <jenkins@jenkins.billa.com>
2023-09-19 19:01:08 +0000

    verion 4

--- src/main/java/com/example/billa/BillaApplication.java
+++ src/main/java/com/example/billa/BillaApplication.java
@@ -5,13 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BillaApplication {
-	@RequestMapping("/")
-        public String home() {
-          return "Abraham AUgustine! Dockerizing Spring Boot Application";
-        }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BillaApplication.class, args);
+                System.out.println("Abraham Augustine! Dockerizing Spring Boot Application ");
 	}
-
 }
